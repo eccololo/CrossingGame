12143de7018b681a445618bf0ec857fd0bad9ea7
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2022-07-25 19:38:13 +0200

    Creating initial body of CarManager class and creating create_cars method.

--- car_manager.py
+++ car_manager.py
@@ -1,7 +1,22 @@
+from turtle import Turtle
+import random
+
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
 class CarManager:
-    pass
+
+    def __init__(self):
+        self.all_cars = []
+
+    def create_cars(self):
+        chance = random.randint(1, 6)
+        if chance == 1:
+            car = Turtle()
+            car.color(random.choice(COLORS))
+            car.shapesize(stretch_wid=1, stretch_len=2)
+            car.penup()
+            self.all_cars.append(car)
+
