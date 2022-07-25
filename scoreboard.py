cf5943dbd3f59b3231de748ebcf933dce56a0ba8
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2022-07-25 19:32:43 +0200

    Writing two additional methods that will display current level and increase it.

--- scoreboard.py
+++ scoreboard.py
@@ -11,4 +11,11 @@ class Scoreboard(Turtle):
         self.penup()
         self.hideturtle()
         self.goto(-290, 250)
+        self.write_current_level()
+
+    def write_current_level(self):
+        self.clear()
         self.write(f"Level: {self.level}", align="left", font=FONT)
+
+    def increase_level(self):
+        self.level += 1
