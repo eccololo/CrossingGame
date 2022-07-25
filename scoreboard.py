10b1d0c1caac1bb6b28983c4f78a15285048f179
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2022-07-25 19:29:39 +0200

    Creating initial body of the Scoreboard class and writing to game screen current game level.

--- scoreboard.py
+++ scoreboard.py
@@ -1,5 +1,14 @@
+from turtle import Turtle
+
 FONT = ("Courier", 24, "normal")
 
 
-class Scoreboard:
-    pass
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.level = 1
+        self.penup()
+        self.hideturtle()
+        self.goto(-290, 250)
+        self.write(f"Level: {self.level}", align="left", font=FONT)
